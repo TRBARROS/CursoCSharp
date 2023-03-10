dff5c0b21b0ee03668e1527a3f9cd994adb8ba21
TRBARROS <thiago_NO1@outlook.com>
2023-03-09 23:56:45 -0300

    EX01

---
+++ EX01/EX01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace EX01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritorip";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos:");
+            Console.WriteLine($"{produto1}, cujo preço é $ {preco1}");
+            Console.WriteLine($"{produto2}, cujo preco é $ {preco2}");
+
+            Console.WriteLine($"{idade} anos de idade, código {codigo} e gênero {genero}");
+
+            Console.WriteLine($"Medida com oito casas decimais: {medida:F8}");
+            Console.WriteLine($"Arredondado (três casas decimais): {medida:F03}");
+            Console.WriteLine($"Separador decimal invariant culture: {medida.ToString("F03", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
