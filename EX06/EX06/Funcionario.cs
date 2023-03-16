149f0e3d552f6ec67a433e76bdc69e13d0dead94
TRBARROS <thiago_NO1@outlook.com>
2023-03-15 23:20:53 -0300

    EX06

---
+++ EX06/EX06/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EX06
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            double PorcentagemDec = porcentagem / 100;
+            double PorcentagemSal = SalarioBruto * PorcentagemDec;
+            double SalarioAumentado = SalarioLiquido() + PorcentagemSal;
+            SalarioBruto = SalarioAumentado;
+        }
+    }
+}
--- EX06/EX06/Program.cs
+++ EX06/EX06/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System;
+using System.Globalization;
+
+namespace EX06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario F1 = new Funcionario();
+
+            Console.WriteLine("Digite o nome do funcionário:");
+            F1.Nome = Console.ReadLine();
+            Console.WriteLine("Digite o salário bruto do funcionário:");
+            F1.SalarioBruto = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite o valor dos impostos:");
+            F1.Imposto = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Funcionário: {F1.Nome}, $ {F1.SalarioLiquido().ToString("F02", CultureInfo.InvariantCulture)}");
+
+            Console.WriteLine("Digite a porcentagem para aumentar o salário:");
+            double Porcentagem = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            F1.AumentarSalario(Porcentagem);
+
+            Console.WriteLine($"Dados atualizados: {F1.Nome}, $ {F1.SalarioBruto.ToString("F02", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
