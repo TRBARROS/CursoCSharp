d64d1f1a6b755e2e4a424664e197965428e72d21
TRBARROS <thiago_NO1@outlook.com>
2023-03-16 23:19:53 -0300

    EX08

---
+++ EX08/EX08/ConversorDeMoeda.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EX08
+{
+    class ConversorDeMoeda
+    {
+        public static double Calculo(Double Cot, Double Qtde)
+        {
+            return (Cot * Qtde) + ((Cot * Qtde) * 0.06);
+        }
+    }
+}
---
+++ EX08/EX08/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace EX08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Cotacao;
+            double Quantidade;
+            double Resultado;
+
+            Console.WriteLine("Qual é a cotação do dólar ?");
+            Cotacao = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Quantos dólares você vai comprar ?");
+            Quantidade = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Resultado = ConversorDeMoeda.Calculo(Cotacao, Quantidade);
+
+            Console.WriteLine($"Valor a ser pago em reais = {Resultado.ToString("F02", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
