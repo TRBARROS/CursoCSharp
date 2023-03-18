057279541926a1455444023695eec637261bf25c
TRBARROS <thiago_NO1@outlook.com>
2023-03-17 23:32:23 -0300

    EX09

---
+++ EX09/EX09/ContaBancaria.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace EX09
+{
+    internal class ContaBancaria
+    {
+        private int _numConta;
+        private string _nome;
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numConta, string nome, double valorInicial) {
+            _numConta = numConta;
+            _nome = nome;
+            Saldo = valorInicial;
+        }
+
+        public int NumConta
+        {
+            get { return _numConta; }
+            set { 
+                if (value != 0)
+                {
+                    _numConta = value;
+                }
+            }
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set {
+                if (_nome != null) { 
+                    _nome = value;
+                }
+            }
+        }
+
+        public void Deposito(double ValorDeposito)
+        {
+            Saldo += ValorDeposito;
+        }
+
+        public void Saque(double ValorSaque)
+        {
+            Saldo = Saldo - ( ValorSaque + 5);
+        }
+
+    }
+}
---
+++ EX09/EX09/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace EX09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Conta;
+            string Nome;
+            double ValorInicial;
+            string DecideCTemVI;
+
+            Console.WriteLine("Entre com o número da conta:");
+            Conta = Convert.ToInt32(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre com o titular da conta");
+            Nome = Console.ReadLine();
+            Console.WriteLine("Haverá depósito inicial (s/n)?");
+            DecideCTemVI = Console.ReadLine().ToLower();
+
+            if (DecideCTemVI == Convert.ToString('s'))
+            {
+                Console.WriteLine("Digite o valor inicial");
+                ValorInicial = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                ValorInicial = 00.00;
+            }
+            ContaBancaria c1 = new ContaBancaria(Conta, Nome, ValorInicial);
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine($"Conta {c1.NumConta}, Titular: {c1.Nome}, Saldo: $ {c1.Saldo.ToString("F02", CultureInfo.InvariantCulture)}");
+
+            Console.WriteLine("Entre com um valor para depósito:");
+            c1.Deposito(Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine("Dados Atualizados:");
+            Console.WriteLine($"Conta {c1.NumConta}, Titular: {c1.Nome}, Saldo: $ {c1.Saldo.ToString("F02", CultureInfo.InvariantCulture)}");
+
+            Console.WriteLine("Entre com um valor para saque");
+            c1.Saque(Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine("Dados atualizados:");
+            Console.WriteLine($"Conta {c1.NumConta}, Titular: {c1.Nome}, Saldo: $ {c1.Saldo.ToString("F02", CultureInfo.InvariantCulture)}");
+
+
+        }
+    }
+}
