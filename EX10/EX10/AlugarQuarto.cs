342b9385d55ecc19e19b0240d476b3a5f0e17674
TRBARROS <thiago_NO1@outlook.com>
2023-03-21 20:35:57 -0300

    EX10

---
+++ EX10/EX10/AlugarQuarto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace EX10
+{
+    internal class AlugarQuarto
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public int NumDoQuarto { get; private set; }
+
+        public AlugarQuarto(string name, string email, int numdoquarto)
+        {
+            Name = name;
+            Email = email;
+            NumDoQuarto = numdoquarto;
+        }
+
+    }
+
+}
---
+++ EX10/EX10/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace EX10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumQuartos;
+            AlugarQuarto[] Vect = new AlugarQuarto[10];
+
+            Console.WriteLine("How many rooms will be rented ?");
+            NumQuartos = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < NumQuartos; i++)
+            {
+                Console.WriteLine($"Rent #{i + 1}");
+                Console.WriteLine("Nome da pessoa a alugar:");
+                string Name = Console.ReadLine();
+                Console.WriteLine("Email da Pessoa que esta alugando:");
+                string Email = Console.ReadLine();
+                Console.WriteLine("Numero do quarto a ser alugado;");
+                Int32 NumDoQuarto = Convert.ToInt32(Console.ReadLine());
+
+                Vect[NumDoQuarto] = new AlugarQuarto(Name, Email, NumDoQuarto);
+            }
+
+            Console.WriteLine("Busy rooms:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (Vect[i] != null)
+                {
+                    Console.WriteLine($"{i}: {Vect[i].Name}, {Vect[i].Email}");
+                }
+            }
+            
+
+        }
+    }
+}
