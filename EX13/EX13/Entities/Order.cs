47700f188ea72f9ea80bd1d65696a69980ae272d
TRBARROS <thiago_NO1@outlook.com>
2023-03-31 00:43:17 -0300

    EX13

---
+++ EX13/EX13/Entities/Order.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using EX13.Entities.Enums;
+
+namespace EX13.Entities
+{
+    class Order
+    {
+        public DateTime Date { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order() 
+        {
+        }
+
+        public Order(DateTime date, OrderStatus status, Client client)
+        {
+            Date = date;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double TotalValue = 0;
+
+            foreach (OrderItem i in Items)
+            {
+                TotalValue += i.SubTotal();
+            }
+
+            return TotalValue;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("ORDER SUMARY: ");
+            sb.AppendLine("Order moment: ");
+            sb.Append(Date);
+            sb.AppendLine("Order status: ");
+            sb.Append(Status);
+            sb.AppendLine("Client: ");
+            sb.Append(Client.Name);
+            sb.Append($" ({Client.BirthDate}) ");
+            sb.Append($"- {Client.Email}");
+            sb.AppendLine("Order items");
+
+            foreach (OrderItem i in Items)
+            {
+                sb.AppendLine($"{i.Product.Name}, {i.Product.Price}, Quantity: {i.Quantity}, Subtotal: ${i.SubTotal()}");
+            }
+
+            sb.AppendLine($"Total price: ${Total()}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ EX13/EX13/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using EX13.Entities.Enums;
+using EX13.Entities;
+
+namespace EX13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name;
+            string Email;
+            DateTime BirthDate;
+            string StatusStr;
+            OrderStatus Status;
+            int Quantity;
+            string ProductName;
+            double ProductPrice;
+            int ProductQtt;
+
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            Name = Console.ReadLine();
+            Console.Write("Email: ");
+            Email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            BirthDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            Status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Console.Write("How many items to this order?");
+            Quantity = Convert.ToInt32(Console.ReadLine());
+
+            Client ClientObj = new Client(Name, Email, BirthDate);
+            Order PedidoObj = new Order(DateTime.Now, Status, ClientObj);
+            Product ProductObj;
+            OrderItem OrderItemObj;
+
+            for (int i = 1; i <= Quantity; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product name: ");
+                ProductName = Console.ReadLine();
+                Console.Write("Product price: ");
+                ProductPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                ProductQtt = Convert.ToInt32(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                ProductObj = new Product(ProductName, ProductPrice);
+                OrderItemObj = new OrderItem(ProductQtt, ProductPrice, ProductObj);
+                PedidoObj.AddItem(OrderItemObj);
+            }
+
+            Console.WriteLine(PedidoObj);
+
+        }
+    }
+}
