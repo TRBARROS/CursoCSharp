8ae4a49018c2b1125ba94b2c1de95a9670834277
TRBARROS <thiago_NO1@outlook.com>
2023-04-17 20:51:04 -0300

    EX17

---
+++ EX17/EX17/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+
+namespace EX17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string NameProduct;
+            double PriceProduct;
+            int QtdProduct;
+            double ValTot;
+            using (StreamReader Sr = new StreamReader(@"C:/Users/thiag/OneDrive/Documentos/CursoCSharp/EX17/Produtos.txt"))
+            {
+                StreamWriter Sw = null;
+                try
+                {
+                    Sw = new StreamWriter(@"C:/Users/thiag/OneDrive/Documentos/CursoCSharp/EX17/out/summary.csv");
+
+                    while (!Sr.EndOfStream)
+                    {
+                        string[] ProdctSeparated = Sr.ReadLine().Split(",");
+                        NameProduct = ProdctSeparated[0];
+                        PriceProduct = double.Parse(ProdctSeparated[1], CultureInfo.InvariantCulture);
+                        QtdProduct = int.Parse(ProdctSeparated[2]);
+
+                        ValTot = PriceProduct * QtdProduct;
+
+                        string Line = NameProduct + "," + ValTot.ToString("F02", CultureInfo.InvariantCulture);
+
+                        Sw.WriteLine(Line);
+                    }
+                }
+                finally
+                {
+                    Sw.Close();
+                }
+            }
+        }
+    }
+}
