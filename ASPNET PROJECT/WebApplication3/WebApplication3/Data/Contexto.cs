7d8a2c5b330821449f4e6f2d502b743ccc9d9958
TRBARROS <thiago_NO1@outlook.com>
2023-05-09 20:32:48 -0300

    Database Conected

---
+++ ASPNET PROJECT/WebApplication3/WebApplication3/Data/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
+
+namespace WebApplication3.Data
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options)
+            :base(options) 
+        { }
+
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
---
+++ ASPNET PROJECT/WebApplication3/WebApplication3/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication3.Models
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Display(Name = "Código")]
+        [Column("Id")]
+        public int Id { get; set; }
+        [Display(Name = "Nome")]
+        [Column("Nome")]
+        public string Nome { get; set; }
+    }
+}
